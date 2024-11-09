556d701999f6936788fa30bd6c2e897beee9441e
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-11-09 18:36:01 +0530

    29.29

--- parity_deals/src/app/(marketing)/page.tsx
+++ parity_deals/src/app/(marketing)/page.tsx
@@ -58,6 +58,16 @@ export default function HomePage() {
           </div>
         </div>
       </section>  
+      <section id="pricing" className=" px-8 py-16 bg-accent/5">
+        <h2 className="text-4xl text-center text-balance font-semibold mb-8">
+          Pricing software which pays for itself 20x over
+        </h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto">
+          {subscriptionTiersInOrder.map(tier => (
+            <PricingCard key={tier.name} {...tier} />
+          ))}
+        </div>
+      </section>
       
       
     </>
