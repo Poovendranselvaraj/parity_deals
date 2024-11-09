22520ee80a001513d405d73f04f3a6dfaf6d7dff
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-11-09 16:42:36 +0530

    18.44

--- parity_deals/src/app/(marketing)/_components/NavBar.tsx
+++ parity_deals/src/app/(marketing)/_components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { BrandLogo } from "@/components/BrandLogo";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";   
 
 export default function NavBar() {
@@ -17,7 +18,12 @@ export default function NavBar() {
             About
           </Link>
           <span className="text-lg">
-
+            <SignedIn>
+                <Link href="/dashboard">Dashboard</Link>
+            </SignedIn>
+            <SignedOut>
+                <SignInButton>Login</SignInButton>
+            </SignedOut>
           </span>
         </nav>
     </header>
