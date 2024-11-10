1aa66e1521e5a868125f846ce6ee2e420c48bb4b
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-11-10 12:24:44 +0530

    40.34

--- parity_deals/src/components/ui/button.tsx
+++ parity_deals/src/components/ui/button.tsx
@@ -11,7 +11,9 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        destructive:
+          accent:
+          "bg-accent text-accent-foreground shadow hover:bg-accent/90",
+          destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
